b3d016f9f2534190c403999a23b9a350380cdf49
Scott Lonis <scott.lonis@outlook.com>
2019-08-17 15:01:32 -0500

    began styling app

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Action = (props) => {
-    return (
-        <div>
-            <button 
-                onClick={props.handlePick}
-                disabled={!props.hasOptions}>What Should I Do?</button>
-        </div>
-    );
-};
+const Action = (props) => (
+    <div>
+        <button 
+            className="big-button"
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}>What Should I Do?</button>
+    </div>
+);
 
 export default Action;
