c6f66d5557e89a967387dbb44be1881dc083cf47
Scott Lonis <scott.lonis@outlook.com>
2019-09-10 13:49:42 -0500

    installed several modules essential to production deployment on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,93 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, 
-        {
-            test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        port: 8080
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// if(process.env.NODE_ENV === 'test') {
+//     require('dotenv').config({path: '.env.test'});
+// } else if (process.env.NODE_ENV === 'development') {
+//     require('dotenv').config({path: '.env.development'});
+// }
+
+module.exports = (env) => {
+    const isProduction = env==='production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: ['babel-polyfill', './src/app.js'],
+        output: {
+            path: path.join(__dirname,'public','dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, 
+            {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap:true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+         ],        
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            port: 8000,
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    
     }
-
 };
+
+
+// const path = require('path');
+
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//         path: path.join(__dirname,'public'),
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [{
+//             loader: 'babel-loader',
+//             test: /\.js$/,
+//             exclude: /node_modules/
+//         }, 
+//         {
+//             test: /\.s?css$/,
+//             use: [
+//                 "style-loader",
+//                 "css-loader",
+//                 "sass-loader"
+//             ]
+//         }]
+//     },
+//     devtool: 'cheap-module-eval-source-map',
+//     devServer: {
+//         contentBase: path.join(__dirname,'public'),
+//         port: 8080
+//     }
+
+// };
